e9d0c48025f0819f0c3b32e92c971d1480d4b4ec
nwbort <30829548+nwbort@users.noreply.github.com>
2026-08-25 16:25:26 +1000

    Remove Commentary page from navbar (#830)

--- merger-tracker/frontend/src/constants/navPages.js
+++ merger-tracker/frontend/src/constants/navPages.js
@@ -9,7 +9,7 @@ export const NAV_PAGES = [
   { label: 'Industries', path: '/industries', shortcut: 'i', inNavbar: true, navOrder: 4, inPalette: true, paletteOrder: 4 },
   { label: 'Parties', path: '/parties', inNavbar: false, inPalette: true, paletteOrder: 5 },
   { label: 'Analysis', path: '/analysis', shortcut: 'a', inNavbar: true, navOrder: 6, inPalette: true, paletteOrder: 6 },
-  { label: 'Commentary', path: '/commentary', shortcut: 'c', inNavbar: true, navOrder: 5, inPalette: true, paletteOrder: 7 },
+  { label: 'Commentary', path: '/commentary', shortcut: 'c', inNavbar: false, inPalette: true, paletteOrder: 7 },
   {
     label: 'Digest',
     navbarLabel: 'Catch me up',
